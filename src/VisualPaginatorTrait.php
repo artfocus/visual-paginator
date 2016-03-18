7c1b3c3662210dba842ad02ebc14f022d1c37d10
Jaroslav Hranička <hranicka@outlook.com>
2016-03-18 13:21:45 +0100

    VisualPaginatorTrait added

---
+++ src/VisualPaginatorTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of visual-paginator.
+ * Copyright © 2016 Jaroslav Hranička <hranicka@outlook.com>
+ */
+
+namespace Artfocus\VisualPaginator;
+
+use Artfocus\VisualPaginator;
+
+trait VisualPaginatorTrait
+{
+
+	/**
+	 * @var VisualPaginator\VisualPaginatorFactory
+	 * @inject
+	 */
+	public $visualPaginatorFactory;
+
+	protected function createComponentPaginator()
+	{
+		return $this->visualPaginatorFactory->create();
+	}
+
+}
